e91889bfd83d2d212a9bd5647237bd9e3f03e214
Nirajan Malla <nirajannitro5@gmail.com>
2022-07-09 20:11:01 +0545

    better naming + create new file to point first and last

---
+++ 02targetingFirstAndLastNode.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node *next;
+} nd;
+
+nd *first;
+nd *last;
+
+void traverse(); // creating global first and last struct pointer and assigning the inital and final node to it,
+// we now don't need to pass any argument in this function because one is same as first after line no 24.
+
+int main()
+{
+    nd *one, *two, *three;
+
+    one = (nd *)malloc(sizeof(nd));
+    two = (nd *)malloc(sizeof(nd));
+    three = (nd *)malloc(sizeof(nd));
+    first = one;  // now first and one is same . i.e first->data is same as one->data
+    last = three; // and now last and three is same
+
+    one->data = 10;
+    one->next = two;
+    two->data = 20;
+    two->next = three;
+    three->data = 30;
+    three->next = NULL;
+    traverse();
+    getch();
+    return 0;
+}
+
+void traverse()
+{
+    int i = 1;
+    nd *temp = first; // so that we don't perform operation directly on first(it shouldnot be changed directly like so)
+    while (temp != NULL)
+    {
+        printf("data of node %d: %d\n", i, temp->data);
+        temp = temp->next;
+        i++;
+    }
+}
